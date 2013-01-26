216cbbfa2730ae4eb9016c1fd6212ad00f3929ed
Brendon John <contact@brendonjohn.com>
2013-01-26 15:55:57 +1300

    Added a basic method for viewing the logs on appfrog through the browser

--- FakeClient.js
+++ FakeClient.js
@@ -1,6 +1,20 @@
 var dgram = require('dgram');
 var message = new Buffer("Simon");
+
 var client = dgram.createSocket("udp4");
-client.send(message, 0, message.length, 41234, "localhost", function(err, bytes) {
-  client.close();
-});
+
+	client.send(message, 0, message.length, 41234, "outbackmeteor.hp.af.cm", function(err, bytes) {
+		client.close();
+	});
+	
+	
+	
+	
+	
+	
+//Report logs
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello from <a href="http://appfog.com">AppFog.com</a>');
+}).listen(process.env.VMC_APP_PORT || 1337, null);
--- outbackserver.js
+++ outbackserver.js
@@ -1,17 +1,43 @@
+var logs = [];
+
 var dgram = require("dgram");
 
 var server = dgram.createSocket("udp4");
 
 server.on("message", function (msg, rinfo) {
-  console.log("server got: " + msg + " from " +
-    rinfo.address + ":" + rinfo.port);
+
+	logs.push(msg);
+
+	console.log("server got: " + msg + " from " +
+	rinfo.address + ":" + rinfo.port);
 });
 
+//receive udp packets from game clients
 server.on("listening", function () {
   var address = server.address();
+  
+  //Check if address is currently playing
+  	//assign id for player if it doesn't yet exist
+  
+  //receive current location of the 
   console.log("server listening " +
       address.address + ":" + address.port);
 });
 
 server.bind(41234);
 
+
+
+//Report logs
+var http = require('http');
+http.createServer(function (req, res) {
+  var logs_list = "";
+  
+  for (int i =0; i<logs.length; i++){
+	  logs_list+= "<li>"+logs[i]+"</li>";
+  }
+  
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  
+  res.end('Hello, this is the log list.... <ul>'+logs_list+' </ul>');
+}).listen(process.env.VMC_APP_PORT || 1337, null);

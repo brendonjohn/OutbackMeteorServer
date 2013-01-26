c9b45afa1b4d5a50925d939a24116315f60fea9d
Brendon John <contact@brendonjohn.com>
2013-01-27 10:50:29 +1300

    updating for aws

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var logs = [];
 var player_list = {};
-var serious_port = process.env.PORT || 8813;
+var serious_port = 8813;
 
 var DEBUG = true;
 var dgram = require("dgram");
@@ -66,4 +66,4 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   
   res.end('Hello, this is the log list.... <ul>'+logs_list+' </ul>');
-}).listen(process.env.PORT || 12345);
+}).listen(12345);

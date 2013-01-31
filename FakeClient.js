3c93f0da130341d17902a215fd32f0da9581333b
Brendon John <contact@brendonjohn.com>
2013-01-31 20:00:24 +1300

    Refactoring for a better implementation
    Going to attempt a multiplayer server that can handle more than 5
    frames a second :p

--- FakeClient.js
+++ FakeClient.js
@@ -1,23 +1,50 @@
-var logs = [];
-var serious_port = 8813;
-var destination = "127.0.0.1"//"ec2-50-18-97-146.us-west-1.compute.amazonaws.com"
+//Variables for initializing
+var destination = {
+	address:"localhost",
+	port: 8813
+};
+var listener = {
+	port:8813
+};
 var dgram = require('dgram');
+var messages = {
+	sent:0,
+	received:0
+};
 
 var client = dgram.createSocket("udp4", function(msg, rinfo){
 	console.log("received: " + msg);
-	
 });
 
-SubmitLocationSequence(10,200);
-
+//For sending test packets to the server
 function SubmitLocationSequence(number, currentLocation){
+	
+	console.log("sending message: "+(++messages['sent']).toString());
+	
 	var message = new Buffer(currentLocation.toString()) ;
-	client.send(message, 0, message.length, serious_port, destination, function(err, bytes) {
+	client.send(message, 0, message.length, destination['port'], destination['address'], function(err, bytes) {
 			if (number>0)
-				SubmitLocationSequence(--number, currentLocation-10);
+				SubmitLocationSequence(--number, currentLocation-0.4);
 			else{
-				client.close();
+				//client.close();
+				console.log("Initiating the send has finished");
 			}		
 	});
 }
 
+
+client.on("listening", function () {
+  var address = client.address();
+  
+  //receive current location of the 
+  console.log("client listening for server response \n" +
+      address.address + ":" + address.port);
+      
+      SubmitLocationSequence(100,1000);
+});
+
+client.on("message", function(msg, rinfo){
+	console.log("client received: "+ msg);
+})
+
+client.bind(listener['port']);
--- main.js
+++ main.js
@@ -1,60 +1,29 @@
-var logs = [];
+var TESTMODE = true;
+
 var player_list = {};
-var serious_port = 8813;
-
-var DEBUG = true;
+var listener_config = {
+	port:8813
+};
+var test_mode = {
+	response: 'localhost',
+	port:8813,
+	message_count:0
+};
 var dgram = require("dgram");
- 
 var server = dgram.createSocket("udp4");
-
-function PlayerListString(players, current_user){
-	var returnString = "";
-	for (var id in players){
-		if (id != current_user){
-			if (returnString.length != 0){
-				returnString += ","+id + "-"+players[id];
-			}
-			else{
-				returnString += id + "-"+players[id];
-			}
-		}
-	}	
-	if (returnString == ""){
-		returnString = "None";
-	}
-	return returnString;
-}
 
 server.on("message", function (msg, rinfo) {
 	var user_id = rinfo.address+":"+rinfo.port;
-	if (DEBUG == true){
-		logs.push((new Date()) + ": origin" + msg
-		+ ", "+ rinfo.address
-		+ ", "+ rinfo.port);	
-		console.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
-	}
-		
-	//Check if address is not yet playing
-	if (player_list[user_id] == null){
-	}
 	
-	//Todo: interpret the message
-	new_location = msg;
+	if (TESTMODE == true){
+		console.log("Received: " + msg);
+	}
 		
 	//set the new location
-	player_list[user_id] = new_location;
+	player_list[user_id] = msg;
 	
 	//The response to the client that has submitted the packet
-	var buf = new Buffer(PlayerListString(player_list, user_id));
-	
-	server.send(buf, 0, buf.length,rinfo.port, rinfo.address, function(err, sent) {
-		//the callback for successfully sending
-		if(DEBUG==true){
-			//console.log("confirmed location: " + player_list[rinfo.address] + ", to: "+rinfo.address);
-		}
-	});
-
-	console.log("location: " + player_list[user_id] + ", for: "+rinfo.address);
+	var buf = new Buffer("hey hey hey: "+(test_mode['message_count']++));
 });
 
 //receive udp packets from game clients
@@ -66,21 +35,4 @@ server.on("listening", function () {
       address.address + ":" + address.port);
 });
 
-server.bind(serious_port);
-
-
-
-//Report logs
-var http = require('http');
-http.createServer(function (req, res) {
-  var logs_list = "";
-  
-  for (var i =0; i<logs.length; i++){
-	  logs_list+= "<li>"+logs[i]+"</li>";
-  }
-  
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  
-  res.end(PlayerListString(player_list , "127.0.0.1:58966")+
-  '<br />Hello, this is the log list.... <ul>'+logs_list+' </ul>');
-}).listen(12345);
+server.bind(listener_config['port']);

9343eb4e6d3b78798419728ada7e208316e5497c
Brendon John <contact@brendonjohn.com>
2013-02-06 23:10:30 +1300

    primitive user management
    a user is assigned a unique id and can have its position stored on the
    server

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
 var TESTMODE = true;
 
-var player_list = {};
-var listener_config = {
+var playerList = {};
+var listenerConfig = {
 	port:8813
 };
 
-var io = require("socket.io").listen(listener_config['port']);
+var io = require("socket.io").listen(listenerConfig['port']);
+var users = 0;
 
 //open socket with game clients
 io.sockets.on("connection", function (socket) {
-  //client has connected
-  
-  socket.on('userdetails', function (details){
-  	//Todo: add player and current location to the player_list
-	socket.broadcast.emit("user connected");
-	console.log("user connected: "+details);
-  });
-  
-  socket.on('gameplay', function(message, locationUpdate){
-  	
-  });
-  
+	//client has connected
+	var uid = users++;
+	
+	socket.on('userdetails', function (location){
+		socket.broadcast.emit("user connect", uid);
+		console.log("user connected: " + uid.toString());
+		playerList[uid] = {
+			x: location.x,
+			y: location.y
+		};
+	});
+	
+	socket.on('gameplay', function(locationUpdate){
+		playerList[uid].x = locationUpdate.x;
+		playerList[uid].y = locationUpdate.y;
+	});
+	
+	socket.on('disconnect', function(){
+		socket.broadcast.emit("user disconnect", uid);
+		delete playerList[uid];
+	});
 });
